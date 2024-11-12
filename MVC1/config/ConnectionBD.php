a3185aa3ad4cd0a7f91263da7b5b3a4f0c8f0fe8
Jonnathan Morales Yate <jonnathanmoralesyate@hotmail.com>
2024-11-12 14:56:59 -0500

    correcion

--- MVC1/config/ConnectionBD.php
+++ MVC1/config/ConnectionBD.php
@@ -18,7 +18,7 @@ class Database{
         $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->db_name, $this->username, $this->password);
         $this->conn->exec("set names utf8");
 
-        echo "Conexion Exitosa";
+        //echo "Conexion Exitosa";
 
     }catch(PDOException $exeption) {
 
@@ -32,45 +32,5 @@ class Database{
 
 
 }
-
-
-
-?>
-<?php
-
-class Database{
-
-    private $host="localhost";
-    private $db_name="persona1";
-    private $username="root";
-    private $password="";
-    public $conn;
-
-
-    public function getConnection() {
-        $this->conn = null;
-    
-
-    try {
-
-        $this->conn = new PDO("mysql:host=". $this->host .";dbname=". $this->db_name, $this->username, $this->password);
-        $this->conn->exec("set names utf8");
-
-        echo "Conexion Exitosa";
-
-    }catch(PDOException $exeption) {
-
-        echo "Error de Conexion: ". $exeption->getMessage();
-
-    }
-
-    return $this->conn;
-
-}
-
-
-}
-
-
 
 ?>
